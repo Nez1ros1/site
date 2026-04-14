ffe593f017a71a5b16b2017153f58acf2cd93260
Nez1ros1 <nez1rosmen@gmail.com>
2026-04-14 22:55:18 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,19 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("Вход выполнен!");
+    window.location.href = "chat.html";
+  } catch (error) {
+    alert("Ошибка входа: " + error.message);
+  }
+});
